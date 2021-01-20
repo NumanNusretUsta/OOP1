e35ecc656661b99a37291cf8f77a948c992f0e42
Numan Nusret Usta <numanusta75@gmail.com>
2021-01-21 00:00:14 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] {"ali" , "veli", "ahmet","mehmet"};
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> {"ali","veli","ahmet","mehmet" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("can");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
